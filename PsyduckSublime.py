7a89e649eb212e6369371b7686ca738da3ff6992
nabijaczleweli <nabijaczleweli@gmail.com>
2015-11-23 17:52:20 +0100

    Add code

---
+++ PsyduckSublime.py
@@ -0,0 +1,34 @@
+import sublime_plugin
+
+PSYDUCK =
+'''
+//                    // //  //
+//         __    ____||_//  //
+//       _/__--~~        ~~~-_
+//      /  /___        ___    \
+//     /  /(  +)      ( + )    |
+//    /  |  ~~~    __  ~~~   _/\/|
+//   |    \  ___.-~  ~-.___  \   /
+//    \    \(     ` '      ~~)|   \
+//     \     )              / |    \
+//      \/   /              \ |    |
+//      /   |               | |    |
+//     |    /               |  \__/
+//     |    \_            _/      |    ___
+//     \      ~----...---~       /_.-~~ _/
+//      \_                      |    _-~
+//        \                    /  _-~
+//         ~-.__             _/--~
+//        _.-~  ~~~-----~~~~~
+//       ~-.-. _-~     /_ ._ \
+//            ~       ~  ~  ~-
+'''
+
+class RandomIntCommand(sublime_plugin.TextCommand):
+	def insert(self, view, generator):
+
+	def run(self, view, **kwargs):
+		sels = self.view.sel()
+
+		for region in sels:
+			self.view.insert(view, region.begin(), PSYDUCK)
