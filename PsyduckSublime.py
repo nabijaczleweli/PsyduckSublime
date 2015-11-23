e55a3692293d085a8aa127ac996b01c380558914
nabijaczleweli <nabijaczleweli@gmail.com>
2015-11-23 18:29:42 +0100

    Delay-load psyduck

--- PsyduckSublime.py
+++ PsyduckSublime.py
@@ -5,12 +5,15 @@ import sublime_plugin
 
 PSYDUCK = ''
 
-with open(os.path.dirname(os.path.realpath(__file__)) + '/psyduck', 'r') as psyduck:
-	PSYDUCK = psyduck.read()
+def load_psyduck():
+	if not PSYDUCK:
+		with open(os.path.dirname(os.path.realpath(__file__)) + '/psyduck', 'r') as psyduck:
+			PSYDUCK = psyduck.read()
 
 
 class PsyduckCommand(sublime_plugin.TextCommand):
 	def run(self, view, **kwargs):
+		load_psyduck()
 		sels = self.view.sel()
 		for region in sels:
 			self.view.insert(view, region.begin(), PSYDUCK)
